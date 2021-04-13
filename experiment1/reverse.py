bae1ac88c470e75e05c20225a225f244a024da7c
LJW <2505214118@qq.com>
2021-04-13 21:51:13 +0800

    用字典对reverse进行修改

--- experiment1/reverse.py
+++ experiment1/reverse.py
@@ -1,16 +1,23 @@
+import time
+start = time.time()
+
 a_list=[]
 b_list=[]
 with open(r'./experiment1/data/words.txt') as f:
 	for line in f:
 		a_list.append(line.strip('\n').split(',')[0])
-for i in a_list:
-    result=i[::-1]
-    if i==result:          #若它反转后与它本身相同，则不输出
-        continue
-    for j in a_list:
-        if result==j and i + '和' + j not in b_list:
-            print(i,'和',j,';')
-            b_list.append(i + '和' + j)
+words_dict = dict(zip(a_list,a_list))
+
+count = 1
+for word in a_list:
+    rev_word=word[::-1]
+    if rev_word in words_dict and rev_word != word and rev_word + '-' + word not in b_list: 
+        b_list.append(word + '-' + rev_word)
+        print(word, '-' , rev_word ,';')
+        count = count + 1
+end = time.time()
+print('耗时' + str(end-start) + '秒')
+print(count)
 
 
 
